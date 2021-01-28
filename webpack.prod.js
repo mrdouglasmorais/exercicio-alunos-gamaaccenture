5d75c55ebedc6e2447e7624ca95e307b12fd2e25
abstartups-oficial <mr.dougladmorais23@gmail.com>
2021-01-27 23:42:58 -0300

    Buildpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
 });
